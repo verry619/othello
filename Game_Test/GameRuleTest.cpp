741f3cb83c90d653e621078c1c5d21bc2fed579c
Junji Horibe <verry619.jh@gmail.com>
2024-04-29 08:03:32 +0900

    Fix InitializeBoardTest

--- Game_Test/GameRuleTest.cpp
+++ Game_Test/GameRuleTest.cpp
@@ -5,10 +5,19 @@
 constexpr unsigned int ROW_MAX = 6;
 constexpr unsigned int COL_MAX = 6;
 
-const DISC EN_ALL_NONE[ROW_MAX][COL_MAX] = { DISC::NONE };
-const DISC EN_ALL_BLACK[ROW_MAX][COL_MAX] = { DISC::BLACK };
-const DISC EN_ALL_WHITE[ROW_MAX][COL_MAX] = { DISC::WHITE };
-const DISC EN_INIT[ROW_MAX][COL_MAX] =
+enum class EN_BOARD_FOR_TEST
+{
+	ALL_NONE,
+	ALL_BLACK,
+	ALL_WHITE,
+	INIT,
+	BW_HALF,
+};
+
+const DISC BOARD_ALL_NONE[ROW_MAX][COL_MAX] = { DISC::NONE };
+const DISC BOARD_ALL_BLACK[ROW_MAX][COL_MAX] = { DISC::BLACK };
+const DISC BOARD_ALL_WHITE[ROW_MAX][COL_MAX] = { DISC::WHITE };
+const DISC BOARD_INIT[ROW_MAX][COL_MAX] =
 {
 	{DISC::NONE,DISC::NONE,DISC::NONE,DISC::NONE,DISC::NONE,DISC::NONE},
 	{DISC::NONE,DISC::NONE,DISC::NONE,DISC::NONE,DISC::NONE,DISC::NONE},
@@ -17,23 +26,128 @@ const DISC EN_INIT[ROW_MAX][COL_MAX] =
 	{DISC::NONE,DISC::NONE,DISC::NONE,DISC::NONE,DISC::NONE,DISC::NONE},
 	{DISC::NONE,DISC::NONE,DISC::NONE,DISC::NONE,DISC::NONE,DISC::NONE},
 };
+const DISC BOARD_BW_HALF[ROW_MAX][COL_MAX] =
+{
+	{DISC::BLACK,DISC::WHITE,DISC::BLACK,DISC::WHITE,DISC::BLACK,DISC::WHITE},
+	{DISC::BLACK,DISC::WHITE,DISC::BLACK,DISC::WHITE,DISC::BLACK,DISC::WHITE},
+	{DISC::BLACK,DISC::WHITE,DISC::BLACK,DISC::WHITE,DISC::BLACK,DISC::WHITE},
+	{DISC::BLACK,DISC::WHITE,DISC::BLACK,DISC::WHITE,DISC::BLACK,DISC::WHITE},
+	{DISC::BLACK,DISC::WHITE,DISC::BLACK,DISC::WHITE,DISC::BLACK,DISC::WHITE},
+	{DISC::BLACK,DISC::WHITE,DISC::BLACK,DISC::WHITE,DISC::BLACK,DISC::WHITE},
+};
 
-TEST(GameRuleTest, InitializeBoardTest001) {
+static const std::map<EN_BOARD_FOR_TEST, DISC const> g_mapBoardForTest =
+{
+	{EN_BOARD_FOR_TEST::ALL_NONE,	BOARD_ALL_NONE[0][0]	},
+	{EN_BOARD_FOR_TEST::ALL_BLACK,	BOARD_ALL_BLACK[0][0]	},
+	{EN_BOARD_FOR_TEST::ALL_WHITE,	BOARD_ALL_WHITE[0][0]	},
+	{EN_BOARD_FOR_TEST::INIT,		BOARD_INIT[0][0]		},
+	{EN_BOARD_FOR_TEST::BW_HALF,	BOARD_BW_HALF[0][0]	},
+};
+
+void InitializeBoardTest_dataset(BOARD_INFO& stBoardInfo_Tested, EN_BOARD_FOR_TEST enIdx, BOARD_INFO& stBoardInfo_Expected)
+{
 	/* tested data */
-	DISC enBoard_Tested[ROW_MAX][COL_MAX];
-	BOARD_INFO stBoardInfo_Tested = { 0,0,&enBoard_Tested[0][0] };
 	stBoardInfo_Tested.enSize = { ROW_MAX,COL_MAX };
 	memcpy(stBoardInfo_Tested.penDiscs,
-		&EN_ALL_NONE[0][0],
+		&g_mapBoardForTest.at(enIdx),
 		sizeof(DISC) * ROW_MAX * COL_MAX);
 
 	/* expected data */
-	DISC enBoard_Expected[ROW_MAX][COL_MAX];
-	BOARD_INFO stBoardInfo_Expected = { 0,0,&enBoard_Expected[0][0] };
 	stBoardInfo_Expected.enSize = { ROW_MAX,COL_MAX };
 	memcpy(stBoardInfo_Expected.penDiscs,
-		&EN_INIT[0][0],
+		&BOARD_INIT[0][0],
 		sizeof(DISC) * ROW_MAX * COL_MAX);
+}
+
+TEST(GameRuleTest, InitializeBoardTest001) {
+	/* data initializing */
+	DISC enBoard_Tested[ROW_MAX][COL_MAX];
+	BOARD_INFO stBoardInfo_Tested = { 0,0,&enBoard_Tested[0][0] };
+	DISC enBoard_Expected[ROW_MAX][COL_MAX];
+	BOARD_INFO stBoardInfo_Expected = { 0,0,&enBoard_Expected[0][0] };
+
+	InitializeBoardTest_dataset(stBoardInfo_Tested, EN_BOARD_FOR_TEST::ALL_NONE, stBoardInfo_Expected);
+
+	/* testing */
+	GameRule::InitializeBoard(stBoardInfo_Tested);
+	EXPECT_EQ(stBoardInfo_Expected.enSize.ucRow, stBoardInfo_Tested.enSize.ucRow);
+	EXPECT_EQ(stBoardInfo_Expected.enSize.ucCol, stBoardInfo_Tested.enSize.ucCol);
+	EXPECT_EQ(0, memcmp(stBoardInfo_Expected.penDiscs, stBoardInfo_Tested.penDiscs, sizeof(DISC) * ROW_MAX * COL_MAX));
+}
+
+TEST(GameRuleTest, InitializeBoardTest002) {
+	/* data initializing */
+	DISC enBoard_Tested[ROW_MAX][COL_MAX];
+	BOARD_INFO stBoardInfo_Tested = { 0,0,&enBoard_Tested[0][0] };
+	DISC enBoard_Expected[ROW_MAX][COL_MAX];
+	BOARD_INFO stBoardInfo_Expected = { 0,0,&enBoard_Expected[0][0] };
+
+	InitializeBoardTest_dataset(stBoardInfo_Tested, EN_BOARD_FOR_TEST::ALL_BLACK, stBoardInfo_Expected);
+
+	/* testing */
+	GameRule::InitializeBoard(stBoardInfo_Tested);
+	EXPECT_EQ(stBoardInfo_Expected.enSize.ucRow, stBoardInfo_Tested.enSize.ucRow);
+	EXPECT_EQ(stBoardInfo_Expected.enSize.ucCol, stBoardInfo_Tested.enSize.ucCol);
+	EXPECT_EQ(0, memcmp(stBoardInfo_Expected.penDiscs, stBoardInfo_Tested.penDiscs, sizeof(DISC) * ROW_MAX * COL_MAX));
+}
+
+TEST(GameRuleTest, InitializeBoardTest003) {
+	/* data initializing */
+	DISC enBoard_Tested[ROW_MAX][COL_MAX];
+	BOARD_INFO stBoardInfo_Tested = { 0,0,&enBoard_Tested[0][0] };
+	DISC enBoard_Expected[ROW_MAX][COL_MAX];
+	BOARD_INFO stBoardInfo_Expected = { 0,0,&enBoard_Expected[0][0] };
+
+	InitializeBoardTest_dataset(stBoardInfo_Tested, EN_BOARD_FOR_TEST::ALL_WHITE, stBoardInfo_Expected);
+
+	/* testing */
+	GameRule::InitializeBoard(stBoardInfo_Tested);
+	EXPECT_EQ(stBoardInfo_Expected.enSize.ucRow, stBoardInfo_Tested.enSize.ucRow);
+	EXPECT_EQ(stBoardInfo_Expected.enSize.ucCol, stBoardInfo_Tested.enSize.ucCol);
+	EXPECT_EQ(0, memcmp(stBoardInfo_Expected.penDiscs, stBoardInfo_Tested.penDiscs, sizeof(DISC) * ROW_MAX * COL_MAX));
+}
+
+TEST(GameRuleTest, InitializeBoardTest004) {
+	/* data initializing */
+	DISC enBoard_Tested[ROW_MAX][COL_MAX];
+	BOARD_INFO stBoardInfo_Tested = { 0,0,&enBoard_Tested[0][0] };
+	DISC enBoard_Expected[ROW_MAX][COL_MAX];
+	BOARD_INFO stBoardInfo_Expected = { 0,0,&enBoard_Expected[0][0] };
+
+	InitializeBoardTest_dataset(stBoardInfo_Tested, EN_BOARD_FOR_TEST::INIT, stBoardInfo_Expected);
+
+	/* testing */
+	GameRule::InitializeBoard(stBoardInfo_Tested);
+	EXPECT_EQ(stBoardInfo_Expected.enSize.ucRow, stBoardInfo_Tested.enSize.ucRow);
+	EXPECT_EQ(stBoardInfo_Expected.enSize.ucCol, stBoardInfo_Tested.enSize.ucCol);
+	EXPECT_EQ(0, memcmp(stBoardInfo_Expected.penDiscs, stBoardInfo_Tested.penDiscs, sizeof(DISC) * ROW_MAX * COL_MAX));
+}
+
+TEST(GameRuleTest, InitializeBoardTest005) {
+	/* data initializing */
+	DISC enBoard_Tested[ROW_MAX][COL_MAX];
+	BOARD_INFO stBoardInfo_Tested = { 0,0,&enBoard_Tested[0][0] };
+	DISC enBoard_Expected[ROW_MAX][COL_MAX];
+	BOARD_INFO stBoardInfo_Expected = { 0,0,&enBoard_Expected[0][0] };
+
+	InitializeBoardTest_dataset(stBoardInfo_Tested, EN_BOARD_FOR_TEST::BW_HALF, stBoardInfo_Expected);
+
+	/* testing */
+	GameRule::InitializeBoard(stBoardInfo_Tested);
+	EXPECT_EQ(stBoardInfo_Expected.enSize.ucRow, stBoardInfo_Tested.enSize.ucRow);
+	EXPECT_EQ(stBoardInfo_Expected.enSize.ucCol, stBoardInfo_Tested.enSize.ucCol);
+	EXPECT_EQ(0, memcmp(stBoardInfo_Expected.penDiscs, stBoardInfo_Tested.penDiscs, sizeof(DISC) * ROW_MAX * COL_MAX));
+}
+
+TEST(GameRuleTest, FlipDiscs) {
+	/* data initializing */
+	DISC enBoard_Tested[ROW_MAX][COL_MAX];
+	BOARD_INFO stBoardInfo_Tested = { 0,0,&enBoard_Tested[0][0] };
+	DISC enBoard_Expected[ROW_MAX][COL_MAX];
+	BOARD_INFO stBoardInfo_Expected = { 0,0,&enBoard_Expected[0][0] };
+
+	InitializeBoardTest_dataset(stBoardInfo_Tested, EN_BOARD_FOR_TEST::BW_HALF, stBoardInfo_Expected);
 
 	/* testing */
 	GameRule::InitializeBoard(stBoardInfo_Tested);
